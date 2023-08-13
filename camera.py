004460052bd97560adda15c365169027354c0d90
Bhavesh Ingale <bhaveshingle25@gmail.com>
2023-08-13 17:59:34 +0530

    Added SVM model for model.py. And completed functionalities.

--- camera.py
+++ camera.py
@@ -1,12 +1,12 @@
 import cv2
 
+
 class Camera:
 
     def __init__(self):
         self.camera = cv2.VideoCapture(0)
         if not self.camera.isOpened():
             raise ValueError("Camera not found")
-
 
         self.width = self.camera.get(cv2.CAP_PROP_FRAME_WIDTH)
         self.height = self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
---
+++ main.py
@@ -0,0 +1,9 @@
+import app
+
+
+def main():
+    app.App()
+
+
+if __name__ == "__main__":
+    main()
